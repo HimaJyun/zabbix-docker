fcbf2e3719db20f2d1af990a06c238e0fd864cf9
HimaJyun <HimaJyun@users.noreply.github.com>
2019-08-03 09:30:58 +0900

    フォント修正

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,52 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-if [ ! -f "./.MYSQL_PASSWORD" ];then
-    openssl rand -base64 18 > "./MYSQL_PASSWORD"
+# ここ変えるならdocker-compose.ymlの変更も忘れずに
+MYSQL_PASSWORD="./mysql/MYSQL_PASSWORD"
+MYSQL_ROOT_PASSWORD="./mysql/MYSQL_ROOT_PASSWORD"
+FONT_FILE="./zabbix/font.ttf"
+# zipで入手可能なttfフォントなら他のフォントでも可
+FONT_URL="https://ipafont.ipa.go.jp/old/ipafont/ipagp00303.php"
+
+if [ "$(whoami)" != root ];then
+    echo "require root"
+    exit 1
 fi
 
-if [ ! -f "./.MYSQL_ROOT_PASSWORD" ];then
-    openssl rand -base64 18 > "./MYSQL_ROOT_PASSWORD"
+# MYSQL_PASSWORD自体を400にしてしまうとコンテナ内のmysqlユーザー(おそらくuid 999)から読み取れなくなる
+# なので、mysqlディレクトリを400にして中のファイルは644にする(ディレクトリ自体はdocker=rootでマウントされるので400でもOK)
+# ｺﾚ ｵﾓｲﾂｲﾀ ｵﾚ ｶｼｺｲ ?
+: "mkdir" && {
+    tmp="$(dirname "${MYSQL_PASSWORD}")"
+    mkdir -pv "${tmp}"
+    chmod -v 400 "${tmp}"
+
+    tmp="$(dirname "${MYSQL_ROOT_PASSWORD}")"
+    mkdir -pv "${tmp}"
+    chmod -v 400 "${tmp}"
+
+    mkdir -pv "$(dirname "${FONT_FILE}")"
+}
+
+if [ ! -f "${MYSQL_PASSWORD}" ];then
+    echo "Generate: ${MYSQL_PASSWORD}"
+    openssl rand -base64 18 > "${MYSQL_PASSWORD}"
+    chmod -v 644 "${MYSQL_PASSWORD}"
+fi
+
+if [ ! -f "${MYSQL_ROOT_PASSWORD}" ];then
+    echo "Generate: ${MYSQL_ROOT_PASSWORD}"
+    openssl rand -base64 18 > "${MYSQL_ROOT_PASSWORD}"
+    chmod -v 644 "${MYSQL_ROOT_PASSWORD}"
+fi
+
+if [ ! -f "${FONT_FILE}" ];then
+    echo "Download: Japanese font"
+    : "download" && {
+        tmp="$(mktemp -d)"
+        curl -L -o "${tmp}/font.zip" "${FONT_URL}"
+        unzip -d "${tmp}/font" "${tmp}/font.zip"
+        # findでttfを探すためフォントの種類が変わっても問題ない
+        find "${tmp}/font" -name "*.ttf" -exec mv -v {} "${FONT_FILE}" \;
+        rm -rv "${tmp}"
+    }
 fi
